63d0644e415d99d1479c9b9371843b89b0ccbfe6
bibasrairockz <bibasrairockz@gmail.com>
2024-03-25 03:46:03 +0000

    commiting

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mcqgenrator',
-    version='0.0.1',
-    author='Bibas Rai',
-    author_email='bibasrairockz@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    name = 'mcqgenrator',
+    version = '0.0.1',
+    author = 'Bibas Rai',
+    author_email = 'bibasrairockz@gmail.com',
+    install_requires = ["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
